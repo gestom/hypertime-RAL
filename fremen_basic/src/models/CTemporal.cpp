1f6405596a9d8e9d2ef0bfb3f71371e1335e9747
Tom Krajnik <tkrajnik@labe.felk.cvut.cz>
2018-02-11 13:54:15 +0000

    Model renaming

--- fremen_basic/src/models/CTemporal.cpp
+++ fremen_basic/src/models/CTemporal.cpp
@@ -11,15 +11,15 @@
 CTemporal* spawnTemporalModel(const char* type,int maxPeriod,int elements,int numClasses)
 {
 	CTemporal *temporalModel;
-	if (type[0] == 'I') temporalModel = new CTimeHist(0);
-	else if (type[0] == 'A') temporalModel = new CTimeAdaptiveHist(1);
-	else if (type[0] == 'F') temporalModel = new CFrelement(2);
-	else if (type[0] == 'M') temporalModel = new CTimeMean(3);
-	else if (type[0] == 'G') temporalModel = new CPerGaM(4);
-	else if (type[0] == 'Z') temporalModel = new CTimeNone(5);
-	else if (type[0] == 'V') temporalModel = new CMises(5);
-	else if (type[0] == 'H') temporalModel = new CHyperTime(5);
-	else if (type[0] == 'P') temporalModel = new CPythonHyperTime(5);
+	if 	(strcmp(type,"Histogram")==0) 		temporalModel = new CTimeHist(0);
+	else if (strcmp(type,"FreMEn")==0) 		temporalModel = new CFrelement(2);
+	else if (strcmp(type,"Mean")==0) 		temporalModel = new CTimeMean(3);
+	else if (strcmp(type,"None")==0) 		temporalModel = new CTimeNone(5);
+	else if (strcmp(type,"NaiveHyperTime")==0)	temporalModel = new CHyperTime(5);
+	else if (strcmp(type,"HyperTime")==0) 		temporalModel = new CPythonHyperTime(5);
+	else if (strcmp(type,"VonMises")==0) 		temporalModel = new CMises(5);
+	else if (strcmp(type,"Adaptive")==0) 		temporalModel = new CTimeAdaptiveHist(1);
+	else if (strcmp(type,"Gaussian")==0) 		temporalModel = new CPerGaM(4);
 	else temporalModel = new CTimeNone(0);
 	temporalModel->init(maxPeriod,elements,numClasses);
 	return temporalModel;
