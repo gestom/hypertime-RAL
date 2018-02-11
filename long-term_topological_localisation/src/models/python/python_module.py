1e26395392bbd1e7a2c3a29e8b8485f9d1163b88
atomousek <atomousek@gmail.com>
2018-02-11 17:24:00 +0100

    array  in array_to_model function coppied, probably solution to one pointer for all instances error

--- long-term_topological_localisation/src/models/python/python_module.py
+++ long-term_topological_localisation/src/models/python/python_module.py
@@ -52,8 +52,12 @@ def python_function_estimate(whole_model, time):
         return whole_model[0][0]  # average
     else:
         freq_p = mdl.one_freq(np.array([[time]]), whole_model[0],
+    #freq_p = mdl.one_freq(np.array([[time]]), whole_model[0],
                               whole_model[1], whole_model[3], whole_model[4],
                               whole_model[2])
+    #print('model: ' + str(whole_model))
+    #print('time: ' + str(time))
+    #print('vystup: ' + str(float(freq_p[0])))
     return float(freq_p[0])
 
 
@@ -177,9 +181,12 @@ def python_function_model_to_array(whole_model):
     return output_array
 
 
-def python_function_array_to_model(input_array):
+def python_function_array_to_model(array):
     """
     """
+    input_array = np.copy(array)
+    #print('jsem v array to model')
+    #print('toto je ten array: ' + str(input_array))
     number_of_parameters = 5
     len_shapes = []
     position = 0  # we do not need the zeroth position information
@@ -222,5 +229,5 @@ def python_function_array_to_model(input_array):
 
     # k transformation
     k = int(parameters[4])
-
+    #print('a toto je vystupni model: ' + str(parameters[0]) + str(parameters[1]) + str(parameters[2]) + str(structure) + str(k))
     return parameters[0], parameters[1], parameters[2], structure, k
